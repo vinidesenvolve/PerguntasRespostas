c2178eb6147080119f72b5dbba9b496199884a07
vinidesenvolve <75989911+vinidesenvolve@users.noreply.github.com>
2020-12-20 17:37:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//teste//importando modulo express
+const express = require("express");
+const app = express();
+
+//definindo ejs como view engine
+app.set('view engine', 'ejs'); 
+
+//definindo rota
+app.get("/", (req, res) => res.render("home"));
+
+//carregamento do servidor com arrow function () =>
+app.listen(8181, () => console.log("Server ON"));   
