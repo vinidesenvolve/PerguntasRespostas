b79115371df05d29350c2356ec1559a8b982bd2d
Vinicius <vinidesenvolve@gmail.com>
2020-12-20 23:57:30 -0300

    usand ejs no html

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ const app = express();
 app.set('view engine', 'ejs'); 
 
 //definindo rota
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => {
+    let nome = "Mano Brown";
+    let estado = "Vida loka";
+    let erroMsg = false;
+    let musicas = [
+        {faixa: "Jesus chorou"},
+        {faixa: "Vida loka II"},
+        {faixa: "Diário de um detento"},
+        {faixa: "Fim de semana no parque"}
+    ];
+
+    res.render("home",
+        {
+            nome,
+            estado,
+            associação: "Racionais Mc's",
+            tretas: 1000,
+            msg: erroMsg,
+            musicas
+        });
+});
 
 //carregamento do servidor com arrow function () =>
 app.listen(8181, () => console.log("Server ON"));   
