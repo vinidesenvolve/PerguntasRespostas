29e1abbf2e4000013cc384ce88970d0213c64f11
Vinicius <vinidesenvolve@gmail.com>
2020-12-21 10:15:25 -0300

    instalando bootstrap

--- index.js
+++ index.js
@@ -5,27 +5,13 @@ const app = express();
 //definindo ejs como view engine
 app.set('view engine', 'ejs'); 
 
-//definindo rota
-app.get("/", (req, res) => {
-    let nome = "Mano Brown";
-    let estado = "Vida loka";
-    let erroMsg = false;
-    let musicas = [
-        {faixa: "Jesus chorou"},
-        {faixa: "Vida loka II"},
-        {faixa: "Diário de um detento"},
-        {faixa: "Fim de semana no parque"}
-    ];
+//definindo o local dos arquivos estáticos na pasta public
+app.use(express.static('public'));
 
-    res.render("home",
-        {
-            nome,
-            estado,
-            associação: "Racionais Mc's",
-            tretas: 1000,
-            msg: erroMsg,
-            musicas
-        });
+//definindo rota
+app.get("/primeira", (req, res) => {
+    //rederizando a pagina home.ejs e passando objetos 
+    res.render("home");
 });
 
 //carregamento do servidor com arrow function () =>
